f76825bb9377cc8a55a5847e8ca93a91fec93d0e
AyoubKhachchab <ayoubkhachchab79@gmail.com>
2025-09-25 11:35:25 +0200

    wail

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom'; // permet de naviguer par programmation vers une autre page sans utiliser un link (redirige vers ...)
 
+import { preview } from '../assets';
+import { getRandomPrompt } from '../utils';
+import { FormField, Loader } from '../components';
 
 const CreatePost = () => {
   return (
---
+++ client/src/utils/index.js
@@ -0,0 +1,5 @@
+import { surpriseMePrompts } from "../constant";
+
+export function getRandomPrompt(prompt){ // cette fonction va nous retourner une invite prompt aléatoire 
+    
+}
